8d611530f9acf0d193feb6fa22a5e20f17af1fd4
Jonathas Kranmer <jonathas@vert-capital.com>
2022-11-24 09:57:55 -0300

    Criacao component VPopUp

--- src/app.vue
+++ src/app.vue
@@ -1,5 +1,5 @@
 <template>
-  <div style="width: 80%; padding-left: 10%">
+  <div style="width: 80%; margin: 0 auto">
     <h1>COMPONENTES</h1>
     <hr />
     <h4>TABELA E PAGINAÇÃO</h4>
@@ -41,6 +41,23 @@
       <v-button icon="info" status="primary">Padrão</v-button>
     </div>
     <br />
+    <VPopUp title="Calendário de eventos" positionContent="bottom center">
+      <template #popup-body>
+        <p>
+          Lorem ipsum dolor sit amet, consectetur adipisicing elit. Corporis
+          ipsum ipsam cupiditate dolorem quibusdam commodi fugit illo suscipit,
+          est beatae? Similique suscipit voluptatem quo ratione quas hic quis
+          molestiae officiis!
+        </p>
+        <p>
+          Lorem ipsum dolor sit amet, consectetur adipisicing elit. Corporis
+          ipsum ipsam cupiditate dolorem quibusdam commodi fugit illo suscipit,
+          est beatae? Similique suscipit voluptatem quo ratione quas hic quis
+          molestiae officiis!
+        </p>
+      </template>
+    </VPopUp>
+
     <hr />
     <h4>TAB</h4>
     <br />
@@ -98,6 +115,7 @@
         >Padrão</v-button
       >
     </form>
+    <hr />
   </div>
 </template>
 
@@ -109,6 +127,7 @@ import VTabContent from "./components/tab/VTabContent.vue";
 import VTabHeader from "./components/tab/VTabHeader.vue";
 import VSelect from "./components/form/select/VSelect.vue";
 import VTag from "./components/tag/VTag.vue";
+import VPopUp from "./components/popUp/VPopUp.vue";
 
 export default defineComponent({
   name: "App",
@@ -121,6 +140,7 @@ export default defineComponent({
     VPagination,
     VSelect,
     VTag,
+    VPopUp,
   },
   data() {
     return {
---
+++ src/components/popUp/VPopUp.vue
@@ -0,0 +1,49 @@
+<template>
+  <div class="v-popup">
+    <div @click="show = !show" class="v-popup--click">
+      <slot name="event-area">INSERIR ICONE AQUI</slot>
+    </div>
+    <div
+      class="v-popup__content"
+      :class="positionContent"
+      v-show="show"
+      :style="`min-width: ${widthContent}`"
+    >
+      <div class="v-popup__content--head">
+        <h3 class="v-popup__content--head-title">{{ title }}</h3>
+        <span class="v-popup__content--head-close" @click="show = !show"
+          >x</span
+        >
+      </div>
+      <div class="v-popup__content--body">
+        <slot name="popup-body"></slot>
+      </div>
+      <div class="v-popup__content--footer">
+        <slot name="popup-footer">
+          <a href="#" target="_blank">Ir para visão completa</a>
+        </slot>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script lang="ts" setup>
+import { ref } from "vue";
+const props = defineProps({
+  title: {
+    type: String,
+    default: "",
+  },
+  positionContent: {
+    type: String,
+    default: "bottom center",
+  },
+  widthContent: {
+    type: String,
+    default: "25rem",
+  },
+});
+const show = ref(false);
+</script>
+
+<style src="./VPopUp.scss" lang="scss"></style>
