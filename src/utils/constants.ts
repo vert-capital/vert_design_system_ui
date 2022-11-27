c358c636d4fd80df257fe6f688644f1ed106a4f5
Debora Castro <debora@vert-capital.com>
2022-11-27 16:27:49 -0300

    constants

---
+++ src/utils/constants.ts
@@ -0,0 +1,8 @@
+export const DATE_TIME_PATTERN = /^\d{4}-\d{2}-\d{2} \d{2}:\d{2}$/;
+export const DATE_PATTERN = /^\d{4}-\d{2}-\d{2}$/;
+export const ITENS_COLOR = {
+  yellow: "#e6cf42",
+  primary: "#00747a",
+  green: "#41d394",
+  red: "#f07363",
+};
